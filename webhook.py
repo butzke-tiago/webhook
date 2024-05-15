7e1756e104a885c9606c15fda1f3ebbaf63970af
Tiago Butzke dos Santos <butzke.tiago@gmail.com>
2024-05-15 16:13:41 -0300

    Added post, and log functionality.

--- webhook.py
+++ webhook.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, make_response
+from json import dumps, loads
+from datetime import datetime
+from os import remove
 
 app = Flask('')
 
@@ -8,3 +11,41 @@ def home():
     resp = make_response({})
     resp.headers["X-AdobeSign-ClientId"]=request.headers.get("X-AdobeSign-ClientId")
     return resp
+
+@app.post('/')
+def hook():
+    try:
+        with open('hook.log', 'a') as l:
+            try:
+                record = {
+                    'time': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
+                    'headers': dict(request.headers),
+                    'body': loads(request.data.decode())
+                }
+                l.write(dumps(record)+'\n')
+            except Exception as e:
+                record = {
+                    'time': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
+                    'error': str(e)
+                }
+                l.write(dumps(record)+'\n')
+                return str(e), 500
+        return ''
+    except Exception as e:
+        return str(e), 500
+
+@app.get('/log')
+def log():
+    try:
+        with open('hook.log', 'r') as l:
+            return l.read()
+    except Exception as e:
+        return ''
+    
+@app.delete('/log')
+def delete():
+    try:
+        remove('hook.log')
+    except:
+        pass
+    return ''
